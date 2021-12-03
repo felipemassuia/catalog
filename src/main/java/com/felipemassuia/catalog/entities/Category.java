df4622c21ebc92f6dbfc760d186b297610cb8215
Felipe Massuia <massuia98.felipe@gmail.com>
2021-12-03 02:58:18 -0300

    Category resource

---
+++ src/main/java/com/felipemassuia/catalog/entities/Category.java
@@ -0,0 +1,14 @@
+package com.felipemassuia.catalog.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Category {
+
+	private Long id;
+	private String name;
+	
+	
+}
