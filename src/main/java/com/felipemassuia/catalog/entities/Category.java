de77dbe0ffc16a112a474e0a39a0d69a763d74b1
Felipe Massuia <massuia98.felipe@gmail.com>
2022-01-16 22:44:10 -0300

    Insert and Update Timestamp

--- src/main/java/com/felipemassuia/catalog/entities/Category.java
+++ src/main/java/com/felipemassuia/catalog/entities/Category.java
@@ -1,9 +1,14 @@
 package com.felipemassuia.catalog.entities;
 
+import java.time.Instant;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -21,6 +26,22 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
 	
 	
 }
